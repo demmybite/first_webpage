247538fdd4527c5fe795aa62cbeb40d2824084f1
demmybite0608 <ademola.dan0608@gmail.com>
2020-03-24 16:26:23 +0100

    Javascript Task

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const myName = "Ademola Adeagbo";
 const myCourses = ["Python", "C#", "Node.js", "Html-Css-Javascript"];
 
 if (myCourses.length % 2 === 0) {
+  console.log(`${myName}: ${myCourses}`);
   for (let i = 1; i <= 200; i++) {
     if (i % 2 === 0) {
       console.log(i);
     }
   }
 } else {
+  console.log(`${myName}: ${myCourses}`);
   for (let i = 1; i <= 200; i++) {
     if (i % 2 !== 0) {
       console.log(i);
